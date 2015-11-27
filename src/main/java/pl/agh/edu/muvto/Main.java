7bd42b88df415c94cc17df53ec0789973bb9f449
mliszcz <liszcz.michal@gmail.com>
2015-11-27 20:12:27 +0100

    loading graph from file

--- src/main/java/pl/agh/edu/muvto/Main.java
+++ src/main/java/pl/agh/edu/muvto/Main.java
@@ -1,8 +1,16 @@
 package pl.agh.edu.muvto;
 
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fj.P;
+import fj.Try;
+import fj.data.Either;
+import fj.data.Stream;
 import pl.agh.edu.muvto.model.MuvtoEdge;
 import pl.agh.edu.muvto.model.MuvtoGraph;
 import pl.agh.edu.muvto.model.MuvtoVertex;
@@ -16,23 +24,47 @@ public class Main
 
     /**
      * Application entry point.
-     * @param args commandline arguments
+     * @param args
      */
     public static void main(String[] args)
     {
-        logger.info("Hello World!");
+        loadGraph("test-graph-01.txt")
+            .bimap(Util.liftVoid(Exception::printStackTrace),
+                   Util.liftVoid(graph -> logger.info(graph.toString())));
+    }
 
-        MuvtoGraph graph = new MuvtoGraph();
+    /**
+     * Loads graph from classpath resource.
+     * @param classpathFile
+     * @return loaded graph or exception
+     */
+    public static Either<Exception, MuvtoGraph>
+        loadGraph(String classpathFile) {
 
-        MuvtoVertex v1 = new MuvtoVertex(0);
-        MuvtoVertex v2 = new MuvtoVertex(1);
-
-        MuvtoEdge e1 = new MuvtoEdge(0, 10, 0, 0.5);
-
-        graph.addVertex(v1);
-        graph.addVertex(v2);
-        graph.addEdge(v1, v2, e1);
-
-        logger.info(graph.toString());
+        return Try.f(() -> {
+            ClassLoader classloader = Main.class.getClassLoader();
+            URI uri = classloader.getResource(classpathFile).toURI();
+            return Stream.stream(Files.readAllLines(Paths.get(uri)))
+                .filter(s -> !s.startsWith("#"))
+                .map(line -> line.split("\\s+"))
+                .zipIndex()
+                .map(entry -> {
+                    String[] row = entry._1();
+                    int index = entry._2();
+                    return P.p(new MuvtoVertex(Integer.parseInt(row[0])),
+                               new MuvtoVertex(Integer.parseInt(row[1])),
+                               new MuvtoEdge(index,
+                                             Integer.parseInt(row[2]),
+                                             Integer.parseInt(row[3]),
+                                             Double.parseDouble(row[4]))
+                              );
+                })
+                .foldLeft((graph, data) -> {
+                    graph.addVertex(data._1());
+                    graph.addVertex(data._2());
+                    graph.addEdge(data._1(), data._2(), data._3());
+                    return graph;
+                }, new MuvtoGraph(MuvtoEdge.class));
+        })._1().toEither();
     }
 }
---
+++ src/main/java/pl/agh/edu/muvto/Util.java
@@ -0,0 +1,25 @@
+package pl.agh.edu.muvto;
+
+import java.util.function.Consumer;
+
+import fj.F;
+import fj.Unit;
+
+/**
+ * Utility functions.
+ */
+public class Util {
+
+    /**
+     * Transforms void function into function returning Unit.
+     * Some functions are designed for side effect only.
+     * @param f
+     * @return
+     */
+    public static <A> F<A, Unit> liftVoid(final Consumer<A> f) {
+        return a -> {
+            f.accept(a);
+            return Unit.unit();
+        };
+    }
+}
